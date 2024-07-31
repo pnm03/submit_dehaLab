7e752df736c3f9ce5df5e983e488fc789e502631
Phạm Như Minh <112544686+pnm03@users.noreply.github.com>
2024-07-31 18:13:22 +0700

    main.js

--- main.js
+++ main.js
@@ -1,101 +1,62 @@
-let c = document.getElementById("container");
+let input = document.getElementById("input");
+let button = document.getElementById("button");
+let text = document.getElementById("text");
+text.textContent = "Nhap so can doan"
+let choilai = 0;
 
-let class_add = document.getElementById("class_add");
-let class_hienthi = document.getElementById("class_hienthi");
-let class_chitiet = document.getElementById("class_chitiet");
-let class_add_1 = document.getElementById("class_add_1");
-
-let check = false;
-let b_addphone = document.getElementById("b_addphone");
-b_addphone.addEventListener("click", () => {
-     if (check == false) {
-          b_addphone.textContent = "BACK";
-          check = true;
-          class_add_1.style.display = "";
-          class_chitiet.style.display = "none";
-          class_hienthi.style.display = "none";
-     }
-     else {
-          b_addphone.textContent = "ADD PHONE";
-          check = false;
-          class_add_1.style.display = "none";
-          class_hienthi.style.display = "";
-     }
-});
-
-let arrPhone = [];
-
-let b_save = document.getElementById("b_save");
-b_save.addEventListener("click", () => {
-     let i1 = document.getElementById("i1");
-     let i2 = document.getElementById("i2");
-     let i3 = document.getElementById("i3");
-     let i4 = document.getElementById("i4");
-     if (i1.value === "" || i2.value === "") {
-          alert("Vui long nhap thong tin");
-     }
-     else {
-          let tam = {
-               ten: i1.value,
-               sdt: i2.value,
-               diachi: i3.value,
-               email: i4.value,
-          }
-          arrPhone.push(tam);
-          b_addphone.textContent = "ADD PHONE";
-          check = false;
-          class_add_1.style.display = "none";
-          class_hienthi.style.display = "";
-          hienthi1();
-          i1.value = "";
-          i2.value = "";
-          i3.value = "";
-          i4.value = "";
-     }
-})
-
-function hienthi1 () {
-     let p = document.createElement("p");
-     p.id = arrPhone.length-1;
-     let img = document.createElement("img");
-     img.src = "https://cdn-icons-png.flaticon.com/128/17279/17279910.png";
-     img.style.height = "20px";
-     img.style.width = "20px";
-     let span = document.createElement("span");
-     span.textContent =  arrPhone[arrPhone.length-1].ten;
-     span.style.paddingLeft = "10px";
-     span.style.fontSize = "22px"
-     p.appendChild(img);
-     p.appendChild(span);
-     class_hienthi.appendChild(p);
-
-     p.addEventListener("click", () => {
-          class_chitiet.style.display = "";
-          class_add.style.display = "none";
-          class_hienthi.style.display = "none";
-          let p1 = document.createElement("p");
-          let p2 = document.createElement("p");
-          let p3 = document.createElement("p");
-          let p4 = document.createElement("p");
-          p1.textContent = "Name: " + arrPhone[Number(p.id)].ten;
-          p2.textContent = "Phone number: " + arrPhone[Number(p.id)].sdt;
-          p3.textContent = "Address: " + arrPhone[Number(p.id)].diachi;
-          p4.textContent = "Email: " + arrPhone[Number(p.id)].email;
-          class_chitiet.appendChild(p1);
-          class_chitiet.appendChild(p2);
-          class_chitiet.appendChild(p3);
-          class_chitiet.appendChild(p4);
-
-
-          let ct_back = document.getElementById("ct_back");
-          ct_back.addEventListener("click", () => {
-               class_chitiet.style.display = "none";
-               class_add.style.display = "";
-               class_hienthi.style.display = "";
-               class_chitiet.removeChild(p1);
-               class_chitiet.removeChild(p2);
-               class_chitiet.removeChild(p3);
-               class_chitiet.removeChild(p4);
-          })
-     })
+function start() {
+  let random = parseInt(Math.random() * 100);
+  console.log(random);
+  let dem = 0;
+  return function() {
+    return {
+      random,
+      dem,
+    }
+  }
 }
+
+let random = start()().random;
+let dem = start()().dem;
+
+button.addEventListener("click", () => {
+  if (choilai == 1) {
+    choilai = 0;
+    random = start()().random;
+    dem = start()().dem;
+    button.textContent = "Doan So";
+    text.textContent = "Nhap so can doan";
+  }
+  else {
+    let value = input.value;
+    if (value == "") {
+      text.textContent = "Vui long nhap so ban doan";
+      text.style.color = "red";
+      input.style.border = "solid 1px red";
+    }
+    else if (value > random) {
+      text.textContent = "So ban doan lon hon";
+      text.style.color = "rgb(240, 143, 160)";
+      input.style.border = "solid 1px rgb(240, 143, 160)";
+      ++dem;
+      input.value = "";
+    }
+    else if (value < random) {
+      text.textContent = "So ban doan nho hon";
+      text.style.color = "rgb(32, 206, 125)";
+      input.style.border = "solid 1px rgb(32, 206, 125)";
+      ++dem;
+      input.value = "";
+    }
+    else if (value == random) {
+      ++dem;
+      text.textContent = "Chuc mung! Ban da thang sau " + dem + " lan dem";
+      text.style.color = "rgb(87, 87, 87)";
+      input.style.border = "solid 1px rgb(87, 87, 87)";
+      input.value = "";
+      choilai = 1;
+      button.textContent = "Choi Lai";
+      input.textContent = "";
+    }
+  }
+})
