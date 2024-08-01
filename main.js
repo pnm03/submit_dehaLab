9c1e889e361dc920a7c997d7b591692244fc23a4
Phạm Như Minh <112544686+pnm03@users.noreply.github.com>
2024-08-01 12:12:45 +0700

    main.js

--- main.js
+++ main.js
@@ -1,62 +1,78 @@
-let input = document.getElementById("input");
-let button = document.getElementById("button");
-let text = document.getElementById("text");
-text.textContent = "Nhap so can doan"
-let choilai = 0;
+let c = document.getElementById("container");
 
-function start() {
-  let random = parseInt(Math.random() * 100);
-  console.log(random);
-  let dem = 0;
-  return function() {
-    return {
-      random,
-      dem,
+let top1 = document.getElementById("top");
+let down1 = document.getElementById("down");
+let left1 = document.getElementById("left");
+let right1 = document.getElementById("right");
+
+
+let vitri_nguoichoi;
+let vitri_vatchoi;
+let diem = 0;
+let o_vatchoi;
+let o_nguoichoi;
+function start () {
+  diem = 0;
+  vitri_nguoichoi = {
+    x: Math.floor(Math.random() * (6 - 1)) + 1,
+    y: Math.floor(Math.random() * (6 - 1)) + 1,
+  }
+  vitri_vatchoi = {
+    x: Math.floor(Math.random() * (6 - 1)) + 1,
+    y: Math.floor(Math.random() * (6 - 1)) + 1,
+  }
+  while (vitri_vatchoi.x == vitri_nguoichoi.x && vitri_vatchoi.y == vitri_nguoichoi.y) {
+    vitri_vatchoi = {
+      x: Math.floor(Math.random() * (6 - 1)) + 1,
+      y: Math.floor(Math.random() * (6 - 1)) + 1,
     }
   }
+  let a = String(vitri_nguoichoi.x) + String(vitri_nguoichoi.y);
+  let b = String(vitri_vatchoi.x) + String(vitri_vatchoi.y);
+  o_nguoichoi = document.getElementById(a);
+  o_vatchoi = document.getElementById(b);
+  o_nguoichoi.style.backgroundColor = "green";
+  o_vatchoi.style.backgroundColor = "red";
 }
 
-let random = start()().random;
-let dem = start()().dem;
-
-button.addEventListener("click", () => {
-  if (choilai == 1) {
-    choilai = 0;
-    random = start()().random;
-    dem = start()().dem;
-    button.textContent = "Doan So";
-    text.textContent = "Nhap so can doan";
-  }
-  else {
-    let value = input.value;
-    if (value == "") {
-      text.textContent = "Vui long nhap so ban doan";
-      text.style.color = "red";
-      input.style.border = "solid 1px red";
-    }
-    else if (value > random) {
-      text.textContent = "So ban doan lon hon";
-      text.style.color = "rgb(240, 143, 160)";
-      input.style.border = "solid 1px rgb(240, 143, 160)";
-      ++dem;
-      input.value = "";
-    }
-    else if (value < random) {
-      text.textContent = "So ban doan nho hon";
-      text.style.color = "rgb(32, 206, 125)";
-      input.style.border = "solid 1px rgb(32, 206, 125)";
-      ++dem;
-      input.value = "";
-    }
-    else if (value == random) {
-      ++dem;
-      text.textContent = "Chuc mung! Ban da thang sau " + dem + " lan dem";
-      text.style.color = "rgb(87, 87, 87)";
-      input.style.border = "solid 1px rgb(87, 87, 87)";
-      input.value = "";
-      choilai = 1;
-      button.textContent = "Choi Lai";
-      input.textContent = "";
-    }
+start();
+top1.addEventListener("click", () => {
+  if (vitri_nguoichoi.x != 1) {
+    o_nguoichoi.style.backgroundColor = "white";
+    let a = String(vitri_nguoichoi.x-1) + String(vitri_nguoichoi.y);
+    vitri_nguoichoi.x--;
+    o_nguoichoi = document.getElementById(a);
+    o_nguoichoi.style.backgroundColor = "green";
   }
 })
+
+down1.addEventListener("click", () => {
+  if (vitri_nguoichoi.x != 5) {
+    o_nguoichoi.style.backgroundColor = "white";
+    let a = String(vitri_nguoichoi.x+1) + String(vitri_nguoichoi.y);
+    vitri_nguoichoi.x++;
+    o_nguoichoi = document.getElementById(a);
+    o_nguoichoi.style.backgroundColor = "green";
+  }
+})
+
+left1.addEventListener("click", () => {
+  if (vitri_nguoichoi.y != 1) {
+    o_nguoichoi.style.backgroundColor = "white";
+    let a = String(vitri_nguoichoi.x) + String(vitri_nguoichoi.y-1);
+    vitri_nguoichoi.y--;
+    o_nguoichoi = document.getElementById(a);
+    o_nguoichoi.style.backgroundColor = "green";
+  }
+})
+
+right1.addEventListener("click", () => {
+  if (vitri_nguoichoi.y != 5) {
+    o_nguoichoi.style.backgroundColor = "white";
+    let a = String(vitri_nguoichoi.x) + String(vitri_nguoichoi.y+1);
+    vitri_nguoichoi.y++;
+    o_nguoichoi = document.getElementById(a);
+    o_nguoichoi.style.backgroundColor = "green";
+  }
+})
+
