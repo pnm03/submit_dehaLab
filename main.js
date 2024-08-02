d250cc532e17f94b833ced9d67165921cce3864f
Phạm Như Minh <112544686+pnm03@users.noreply.github.com>
2024-08-02 16:42:35 +0700

    main.js

--- main.js
+++ main.js
@@ -1,208 +1,133 @@
 let c = document.getElementById("container");
 
-let top1 = document.getElementById("top");
-let down1 = document.getElementById("down");
-let left1 = document.getElementById("left");
-let right1 = document.getElementById("right");
-let diem1 = document.getElementById("diem");
-let bo_button = document.getElementById("bo_button");
-let reset = document.getElementById("reset");
+let khung_choi = document.getElementById("khung_choi");
+let vat_roi = document.getElementById("vat_roi");
+let nguoi_choi = document.getElementById("nguoi_choi");
+let diem_so = document.getElementById("diem_so");
+let b_play = document.getElementById("b_play");
 
-
-function start () {
-  let start1 = 0; // 0 = chua ket thuc, 1 = ket thuc
-  let vitri_nguoichoi = 0;
-  let vitri_vatchoi = 0;
-  let diem = 0;
-  let o_vatchoi;
-  let o_nguoichoi;
-  bo_button.style.display = "";
-  reset.style.display = "none";
-  diem = 0;
-  vitri_nguoichoi = {
-    x: 4,
-    y: 1,
-  }
-  vitri_vatchoi = {
-    x: Math.floor(Math.random() * (6 - 1)) + 1,
-    y: Math.floor(Math.random() * (6 - 1)) + 1,
-  }
-  while (vitri_vatchoi.x == vitri_nguoichoi.x && vitri_vatchoi.y == vitri_nguoichoi.y) {
-    vitri_vatchoi = {
-      x: Math.floor(Math.random() * (6 - 1)) + 1,
-      y: Math.floor(Math.random() * (6 - 1)) + 1,
-    }
-  }
-  let a = String(vitri_nguoichoi.x) + String(vitri_nguoichoi.y);
-  let b = String(vitri_vatchoi.x) + String(vitri_vatchoi.y);
-  o_nguoichoi = document.getElementById(a);
-  o_vatchoi = document.getElementById(b);
-  o_nguoichoi.style.backgroundColor = "green";
-  o_vatchoi.style.backgroundColor = "red";
-  let tg = 1000;
-  function andiem() {
-    if (vitri_vatchoi.x == vitri_nguoichoi.x && vitri_vatchoi.y == vitri_nguoichoi.y) {
-      diem++;
-      diem1.textContent = "Diem: " + diem;
-      vitri_vatchoi = {
-        x: Math.floor(Math.random() * (6 - 1)) + 1,
-        y: Math.floor(Math.random() * (6 - 1)) + 1,
-      }
-      while (vitri_vatchoi.x == vitri_nguoichoi.x && vitri_vatchoi.y == vitri_nguoichoi.y) {
-        vitri_vatchoi = {
-          x: Math.floor(Math.random() * (6 - 1)) + 1,
-          y: Math.floor(Math.random() * (6 - 1)) + 1,
-        }
-      }
-      let b = String(vitri_vatchoi.x) + String(vitri_vatchoi.y);
-      o_vatchoi = document.getElementById(b);
-      o_vatchoi.style.backgroundColor = "red";
-    }
-    if (diem >= 10) {
-      tg = 850;
-    }
-    else if (diem >= 20) {
-      tg = 680
-    }
-    else if (diem >= 35) {
-      tg = 500;
-    }
-    else if (diem >= 55) {
-      tg = 300;
-    }
-  }
-  
-  function dilen() {
-    check1 = 2;
-    if (start1 == 0) {
-      if (vitri_nguoichoi.x == 1) {
-        start1 = 1;
-      }
-      if (vitri_nguoichoi.x > 1) {
-        o_nguoichoi.style.backgroundColor = "white";
-        let a = String(vitri_nguoichoi.x-1) + String(vitri_nguoichoi.y);
-        vitri_nguoichoi.x--;
-        o_nguoichoi = document.getElementById(a);
-        o_nguoichoi.style.backgroundColor = "green";
-        andiem();
-      }
-    }
-  }
-  
-  function dixuong () {
-    check1 = 3;
-    if (start1 == 0) {
-      if (vitri_nguoichoi.x == 5) {
-        start1 = 1;
-      }
-      if (vitri_nguoichoi.x < 5) {
-        o_nguoichoi.style.backgroundColor = "white";
-        let a = String(vitri_nguoichoi.x+1) + String(vitri_nguoichoi.y);
-        vitri_nguoichoi.x++;
-        o_nguoichoi = document.getElementById(a);
-        o_nguoichoi.style.backgroundColor = "green";
-        andiem();
-      }
-    }
-  }
-  
-  function disangtrai () {
-    check1 = 1;
-    if (start1 == 0) {
-      if (vitri_nguoichoi.y == 1) {
-        start1 = 1;
-      }
-      if (vitri_nguoichoi.y > 1) {
-        o_nguoichoi.style.backgroundColor = "white";
-        let a = String(vitri_nguoichoi.x) + String(vitri_nguoichoi.y-1);
-        vitri_nguoichoi.y--;
-        o_nguoichoi = document.getElementById(a);
-        o_nguoichoi.style.backgroundColor = "green";
-        andiem();
-      }
-    }
-  }
-  
-  function disangphia () {
-    check1 = 0;
-    if (start1 == 0) {
-      if (vitri_nguoichoi.y == 5) {
-        start1 = 1;
-      }
-      if (vitri_nguoichoi.y < 5) {
-        o_nguoichoi.style.backgroundColor = "white";
-        let a = String(vitri_nguoichoi.x) + String(vitri_nguoichoi.y+1);
-        vitri_nguoichoi.y++;
-        o_nguoichoi = document.getElementById(a);
-        o_nguoichoi.style.backgroundColor = "green";
-        andiem();
-      }
-    }
-  }
-  
-  
-  let check1 = 0;
-  
-  // 0 -> ; 1 <- ; 2 ^ ; 3 v ;
-  let g = setInterval(() => {
-    if (check1 == 0) disangphia();
-    else if (check1 == 1) disangtrai();
-    else if (check1 == 2) dilen();
-    else if (check1 == 3) dixuong();
-    if (start1 == 1) {
-      diem1.textContent = "Tro choi ket thuc. ban duoc " + diem + " diem";
-      clearInterval(g);
-      bo_button.style.display = "none";
-      reset.style.display = "";
-      o_nguoichoi.style.backgroundColor = "white";
-      o_vatchoi.style.backgroundColor = "white";
-    }
-  }, tg)
-  
-  top1.addEventListener("click", () => {
-    if (check1 != 2) dilen();
-  })
-  
-  document.addEventListener('keydown', function(event) {
-    if (event.key === 'w' || event.key === 'W') {
-      if (check1 != 2) dilen();
-    }
-  });
-  
-  down1.addEventListener("click", () => {
-    if (check1 != 3) dixuong();
-  })
-  
-  document.addEventListener('keydown', function(event) {
-    if (event.key === 's' || event.key === 'S') {
-      if (check1 != 3) dixuong();
-    }
-  });
-  
-  left1.addEventListener("click", () => {
-    if (check1 != 1) disangtrai();
-  })
-  
-  document.addEventListener('keydown', function(event) {
-    if (event.key === 'a' || event.key === 'A') {
-      if (check1 != 1) disangtrai();
-    }
-  });
-  
-  right1.addEventListener("click", () => {
-    if (check1 != 0) disangphia();
-  })
-  
-  document.addEventListener('keydown', function(event) {
-    if (event.key === 'd' || event.key === 'D') {
-      if (check1 != 0) disangphia();
-    }
-  });
-  
-  reset.addEventListener("click", () => {
-    location.reload();
-    c.style.display = "none";
-  })
+// ham random vi tri cua vat_choi
+function random_rong () {
+  return Math.floor(Math.random() * (5 - 0)) + 0;
 }
 
-start()
+// ham tra ve vi tri cua nguoi_choi
+function r_left (phantu, value = 0) {
+  if (value == 0) {
+    let a = phantu.style.left;
+    let num = a.indexOf("p");
+    a = a.slice(0, num);
+    return Number(a);
+  }
+  else {
+    let a = phantu.style.left;
+    let num = a.indexOf("p");
+    a = Number(a.slice(0, num));
+    a += value;
+    phantu.style.left = String(a) + "px";
+  }
+}
+function r_bot (phantu, value = 0) {
+  if (value == 0) {
+    let a = phantu.style.bottom;
+    let num = a.indexOf("p");
+    a = a.slice(0, num);
+    return Number(a);
+  }
+  else {
+    let a = phantu.style.bottom;
+    let num = a.indexOf("p");
+    a = Number(a.slice(0, num));
+    a += value;
+    phantu.style.bottom = String(a) + "px";
+  }
+}
+
+function start_game () {
+  khung_choi.style.color = "black";
+  b_play.style.display = "none";
+  let diem_game = 0;
+  const o_vuong = 50;
+  vat_roi.style.left = String(random_rong() * o_vuong) + "px";
+
+  let lv = 10; // toc do roi
+  let auto_roi = setInterval (() => {
+    if (r_bot(vat_roi) >= 0) {
+      if (r_bot(nguoi_choi) - r_bot(vat_roi) > 10) {
+        let a = setInterval (() => {
+          if (r_bot(vat_roi) > 0) {
+            r_bot(vat_roi, -1);
+          }
+          else {
+            khung_choi.style.color = "red";
+            b_play.style.display = "";
+            bd();
+          }
+        }, 1);
+      }
+      if (r_bot(vat_roi) <= r_bot(nguoi_choi) && r_left(vat_roi) == r_left(nguoi_choi)) {
+        vat_roi.style.left = String(random_rong() * o_vuong) + "px";
+        vat_roi.style.bottom = "375px";
+        diem_game++;
+        diem_so.textContent = "Diem: " + diem_game;
+        if (diem_game > 10) {
+          lv = 8;
+        }
+        else if (diem_game > 20) {
+          lv = 6;
+        }
+        else if (diem_game > 35) {
+          lv = 4;
+        }
+        else if (diem_game > 55) {
+          lv = 2;
+        }
+        else if (diem_game > 80) {
+          lv = 1;
+        }
+      } 
+      r_bot(vat_roi, -1);
+    }
+    else {
+      khung_choi.style.color = "red";
+      b_play.style.display = "";
+      bd();
+    }
+  }, lv);
+
+  // dichuyen 
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'w' || event.key === 'W') {
+      // di len
+      if (r_bot(nguoi_choi) <= 100) {
+        r_bot(nguoi_choi, 20);
+      }
+    }
+    if (event.key === 's' || event.key === 'S') {
+      // di xuong
+      if (r_bot(nguoi_choi) > 0) {
+        r_bot(nguoi_choi, -20);
+      }
+    }
+    if (event.key === 'a' || event.key === 'A') {
+      // di sang trai
+      if (r_left(nguoi_choi) > 0) {
+        r_left(nguoi_choi, -o_vuong);
+      }
+    }
+    if (event.key === 'd' || event.key === 'D') {
+      // di sang phai
+      if (r_left(nguoi_choi) < 200) {
+        r_left(nguoi_choi, o_vuong);
+      }
+    }
+  });
+}
+
+function bd () {
+  location.reload();
+}
+
+b_play.addEventListener("click", () => {
+  start_game();
+})
